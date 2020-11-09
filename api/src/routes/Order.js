8f620f48b56fdb02a3255061df72fc9b350e59d4
unknown <facundosadava1@gmail.com>
2020-11-09 20:15:56 -0300

    'route'

--- api/src/routes/Order.js
+++ api/src/routes/Order.js
@@ -4,20 +4,24 @@ const { Order } = require('../db.js');
 //POST --> Generar o buscar una order
 
 server.post('/', (req, res, next) => {
-  console.log("Lo que me llega como body es: ",req.body)
+  console.log("Lo que me llega como body es: ", req.body)
   Order.findOne({
-      where: {
-        clientId: req.body.clientId,
-        status: "true"
-      }
-  }).then(order =>{
+    where: {
+      clientId: req.body.clientId,
+      status: "true"
+    }})
+    .then(order => {
       if(!order) {
         Order.create({
-              clientId: req.body.clientId,
-              status: "true"
-            }).then(created => res.status(200).send(created))
-      }else
-      return res.status(201).send(order)
+          clientId: req.body.clientId,
+          status: "true"
+        })
+    .then(created =>
+      res.status(200)
+      .send(created)
+      )}
+      else
+        return res.status(201).send(order)
   }).catch(next);
 });
 
