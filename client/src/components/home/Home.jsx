39c51d252759e77b2b25f2823fbcb1e95558ca8b
Cesar2016 <cesars.pro@gmail.com>
2020-11-11 09:20:08 -0300

    Paso

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -5,6 +5,7 @@ import Breadcrumbs from '@material-ui/core/Breadcrumbs';
 import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import Swal from 'sweetalert2/src/sweetalert2.js'
 
 
 // tables
